1f475ab9fb8edb3d02e45bd7fa10f4a38d5a1061
Nathan Onofeghara <nathanonofeghara@gmail.com>
2022-09-14 11:49:05 +0100

    finished usercontroller file to handle login and signup of the users

---
+++ server/controllers/userController.js
@@ -0,0 +1,48 @@
+const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+
+
+// Create token
+const createToken = (_id) => {
+   return jwt.sign({ _id }, process.env.SECRET_TOKEN, {expiresIn: '3d'})
+}
+
+
+
+// Login user
+const loginUser = async (req, res) => {
+    const { email, password} = req.body
+
+    try {
+        const user = await User.login(email, password);
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+
+    } catch(error){
+        res.status(400).json({ error: error.message })
+    }
+
+}
+
+// Signup user
+const signupUser = async (req, res) => {
+    const {email, password} = req.body
+
+    try{
+        const user = await User.signup(email, password);
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch(error){
+        res.status(400).json({ error: error.message })
+    }
+
+}
+
+
+module.exports = { signupUser, loginUser}
