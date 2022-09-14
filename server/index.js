389008adb0b4251cb9cfb303c5cc2227fdb5ecc2
Nathan Onofeghara <nathanonofeghara@gmail.com>
2022-09-14 11:57:02 +0100

    added dotenv support and express built-in body-parser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require('express'),
-      app     = express(),
-      port    = process.env.PORT || 4000, 
-      cors       = require('cors')
+require('dotenv').config({path: __dirname + '/.env'});
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT || 4000;
+const cors    = require('cors');
 
 
 
@@ -10,16 +11,20 @@ app.use(cors({
     origin: '*'
 }));
 
+app.use(express.json())
+
 
     //   Required routes
-const campgroundRoutes = require('./routes/campgrounds'),
-      commentRoutes    = require('./routes/comments')
+const campgroundRoutes = require('./routes/campgrounds');
+const commentRoutes    = require('./routes/comments');
+const userRoutes    = require('./routes/user');
 
 
 
     //  Used reoutes
 app.use('/api/campgrounds', campgroundRoutes)
 app.use('/api/comments', commentRoutes)
+app.use('/api/user', userRoutes)
 
 
 
